5af263213a5452eba006e2750597866fc0878149
jiahua <littleguner@gmail.com>
2024-11-12 16:45:59 +0800

    refactor

--- pkg/sinks/clickhouse/table.sql
+++ pkg/sinks/clickhouse/table.sql
@@ -7,12 +7,10 @@ CREATE TABLE
     IF NOT EXISTS database.table (
         `_id` String,
         `doc` JSON,
-        `__date` Date MATERIALIZED toDate (
-            reinterpretAsInt64 (reverse (unhex (substring(_id, 1, 8))))
-        ),
+        `__date` Date,
         `__ver` UInt64 DEFAULT 0, -- version, derived from oplog timestamp or _id timestamp
         `__is_deleted` UInt8 DEFAULT 0, -- 0:未删除 1:已删除 默认值为 0
-        `__ns` String, -- namespace
+        `__ns` String DEFAULT '', -- namespace
         `__op_time` UInt64 DEFAULT 0, -- for tracing oplog
         `__sync_time` UInt64 DEFAULT 0 -- for tracing
     ) ENGINE = ReplacingMergeTree (`__ver`, `__is_deleted`)
